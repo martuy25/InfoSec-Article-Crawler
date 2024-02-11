b0fff2c8fb4838df3f8720ed94c814486bb723b9
martuy25 <133281613+martuy25@users.noreply.github.com>
2024-02-11 16:09:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import sqlite3
-from article_script import grab_articles, parse_articles, store_articles, search_articles
+from article_script import grab_Krebs_articles, parse_Krebs_articles, store_Krebs_articles, search_Krebs_articles
+from article_script import grab_hacker_news_articles, parse_hacker_news_article, store_hacker_news_articles, search_hacker_news_articles
 
 def main():
+    conn = None
     try:
-        target_url = "https://krebsonsecurity.com/" #specifies url
-        articles_content = grab_articles(target_url) #gets website content
-        articles = parse_articles(articles_content) #parses content for extraction
+        krebs_url = "https://krebsonsecurity.com/" # specifies URL
+        krebs_content = grab_Krebs_articles(krebs_url) # gets website content
+        krebs_articles = parse_Krebs_articles(krebs_content) # parses content for extraction
+        
+        hacker_news_url = "https://thehackernews.com/"
+        hacker_news_content = grab_hacker_news_articles(hacker_news_url)
+        #print("hacker_news_content:", hacker_news_content)
+        hacker_news_articles = parse_hacker_news_article(hacker_news_content)
         
         conn = sqlite3.connect("articles.db")
-        store_articles(articles, conn)
+        store_Krebs_articles(krebs_articles, conn) # store Krebs articles to db
+        store_hacker_news_articles(hacker_news_articles, conn) # store hackernews articles to db
         
-        search_articles(conn, "Google")
-        search_articles(conn, "Hacker")
-        search_articles(conn, "Bitcoin")
+        # Searching Krebs
+        search_Krebs_articles(conn, "Google")
+        search_Krebs_articles(conn, "Hacker")
+        search_Krebs_articles(conn, "Bitcoin")
+        
+        # Searching hacker news
+        search_hacker_news_articles(conn, "Raspberry")
+        search_hacker_news_articles(conn, "OpenAI")
+        search_hacker_news_articles(conn, "Android")
         
     except Exception as e:
         print("An error occurred:", e)
 
     finally:
-        conn.close()
+    	if conn is not None:
+    		conn.close()
 
 if __name__ == "__main__":
     main()
